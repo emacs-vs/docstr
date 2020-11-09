b5e4cb5a40388fe334aa2360e24f7ea260f8c04b
JenChieh <jcs090218@gmail.com>
2020-11-09 22:29:32 +0800

    Add util scripts for development.

---
+++ _scripts/clean.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: clean.bat $
+:: $Date: 2019-10-10 15:59:50 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2019 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+del /s *.elc
---
+++ _scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ========================================================================
+# $File: clean.sh $
+# $Date: 2019-10-17 17:53:18 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+cd ..
+
+find . -name "*.elc" -type f -delete
