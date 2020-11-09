d958bdebd16e81210f1d2e48ff4919209d7b6c4d
JenChieh <jcs090218@gmail.com>
2020-11-09 21:34:00 +0800

    Update core.

--- docstr.el
+++ docstr.el
@@ -32,11 +32,63 @@
 
 ;;; Code:
 
+(require 'docstr-parsers)
+
 (defgroup docstr nil
   "A document string minor mode."
   :prefix "docstr-"
   :group 'tool
   :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/docstr"))
+
+(defun docstr-actionscript-parser (search-string)
+  "Parser for ActionScript using SEARCH-STRING."
+  )
+
+(defun docstr-major-modes ()
+  "List of `major-mode' that supports document string."
+  (let (lst)
+    (dolist (m docstr-parser-alist) (push (car m) docstr-parser-alist))
+    (reverse lst)))
+
+
+(defun docstr--trigger-return ()
+  "Trigger document string by pressing key return."
+  )
+
+(defun docstr--trigger-csharp ()
+  "Trigger document string inside C#."
+  )
+
+(defun docstr--trigger-lua ()
+  "Trigger document string inside Lua."
+  )
+
+(defun docstr--trigger-python ()
+  "Trigger document string inside Python."
+  )
+
+(defun docstr--enable ()
+  "Enable `docstr' in current buffer."
+  (advice-add (key-binding (kbd "RET")) :after #'docstr--trigger-return)
+  (cl-case major-mode
+    (csharp-mode (advice-add (key-binding (kbd "/")) :after #'docstr--trigger-csharp))
+    (lua-mode (advice-add (key-binding (kbd "-")) :after #'docstr--trigger-lua))
+    (python-mode (advice-add (key-binding (kbd "\"")) :after #'docstr--trigger-python))))
+
+(defun docstr--disable ()
+  "Disable `docstr' in current buffer."
+  (advice-remove (key-binding (kbd "RET")) #'docstr--trigger-return)
+  (cl-case major-mode
+    (csharp-mode (advice-remove (key-binding (kbd "/")) #'docstr--trigger-csharp))
+    (lua-mode (advice-remove (key-binding (kbd "-")) #'docstr--trigger-lua))
+    (python-mode (advice-remove (key-binding (kbd "\"")) #'docstr--trigger-python))))
+
+;;;###autoload
+(define-minor-mode docstr-mode
+  "Minor mode 'docstr-mode'."
+  :lighter " DocStr"
+  :group docstr
+  (if docstr-mode (docstr--enable) (docstr--disable)))
 
 (provide 'docstr)
 ;;; docstr.el ends here
