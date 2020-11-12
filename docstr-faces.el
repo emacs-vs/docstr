3b5c9bb5882403ac39e81436b1eb997d6be23354
JenChieh <jcs090218@gmail.com>
2020-11-12 23:32:10 +0800

    Rename faces.

--- docstr-faces.el
+++ docstr-faces.el
@@ -24,37 +24,39 @@
 
 ;;; Code:
 
-(defface docstr-tag-face
+(require 'docstr)
+
+(defface docstr-faces-tag-face
   '((t (:foreground "SlateGray")))
   "Highlighting for Docstring tag."
   :group 'docstr)
-(defvar docstr-tag-face 'docstr-tag-face)
+(defvar docstr-faces-tag-face 'docstr-faces-tag-face)
 
-(defface docstr-type-face
+(defface docstr-faces-type-face
   '((t (:foreground "SteelBlue")))
   "Highlighting for Docstring type."
   :group 'docstr)
-(defvar docstr-type-face 'docstr-type-face)
+(defvar docstr-faces-type-face 'docstr-faces-type-face)
 
-(defface docstr-value-face
+(defface docstr-faces-value-face
   '((t (:foreground "gold4")))
   "Highlighting for Docstring value."
   :group 'docstr)
-(defvar docstr-value-face 'docstr-value-face)
+(defvar docstr-faces-value-face 'docstr-faces-value-face)
 
-(defun docstr-apply-faces ()
+(defun docstr-faces-apply ()
   "Apply standard document string faces."
   (dolist (mode (docstr-major-modes))
     (font-lock-add-keywords
      mode
      '(;; `@param` { typename } val-tag : value tag description..
-       ("\\(?:^\\|\\s-\\)\\(@[^ \"'{}()\t\r\n]+\\)" 1 'docstr-tag-face t)
+       ("\\(?:^\\|\\s-\\)\\(@[^ \"'{}()\t\r\n]+\\)" 1 'docstr-faces-tag-face t)
        ;; @param `{ typename }` val-tag : value tag description..
-       ("[ \t]+@[^ \t\r\n]+\\(?:^\\|\\s-\\)\\([\\[{][^}]*.\\)" 1 'docstr-type-face t)
+       ("[ \t]+@[^ \t\r\n]+\\(?:^\\|\\s-\\)\\([\\[{][^}]*.\\)" 1 'docstr-faces-type-face t)
        ;; @param { typename } `val-tag` : value tag description..
-       ("[ \t]+@[^ \t\r\n].*[\]\|}]\\([^\r\n]*\\)[:-]" 1 'docstr-value-face t)
+       ("[ \t]+@[^ \t\r\n].*[\]\|}]\\([^\r\n]*\\)[:-]" 1 'docstr-faces-value-face t)
        ;; @param `val-tag` : value tag description..
-       ("[ \t]+@[^ \t\r\n]*[ \t]*\\([a-zA-Z0-9_.*&]*\\)[ \t\n]*[{:-]" 1 'docstr-value-face t))
+       ("[ \t]+@[^ \t\r\n]*[ \t]*\\([a-zA-Z0-9_.*&]*\\)[ \t\n]*[{:-]" 1 'docstr-faces-value-face t))
      'end)))
 
 (provide 'docstr-faces)
