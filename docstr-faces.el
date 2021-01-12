d180eb8ad8f7c5a6b04267d8e515ec90cbe3984f
Jen-Chieh <jcs090218@gmail.com>
2021-01-12 13:24:50 +0800

    Update license year

--- docstr-faces.el
+++ docstr-faces.el
@@ -1,6 +1,6 @@
 ;;; docstr-faces.el --- Faces for document string  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020  Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Copyright (C) 2020-2021  Shen, Jen-Chieh <jcs090218@gmail.com>
 
 ;; This file is NOT part of GNU Emacs.
 
