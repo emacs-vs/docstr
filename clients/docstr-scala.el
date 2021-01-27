41c8c7651d5d923fa60973a76217d088611de43e
JenChieh <jcs090218@gmail.com>
2021-01-28 01:00:14 +0800

    Impls scala

--- clients/docstr-scala.el
+++ clients/docstr-scala.el
@@ -26,23 +26,45 @@
 
 (require 'docstr)
 
-(declare-function docstr-writers-rust "ext:docstr-rust.el")
-
-(defcustom docstr-scala-style nil
+(defcustom docstr-scala-style 'scaladoc
   "Style specification for document string in Scala."
-  :type '(choice (const :tag "No specify" nil))
+  :type '(choice (const :tag "No specify" nil)
+                 (const :tag "Scaladoc" scaladoc))
   :group 'docstr)
+
+(defcustom docstr-scala-prefix "* "
+  "Prefix you use on each newline."
+  :type 'string
+  :group 'docstr)
+
+(defun docstr-scala-config-scaladoc ()
+  "Configure for convention, Scaladoc."
+  (docstr-util-default-format)
+  (setq-local docstr-scala-prefix "* "
+              docstr-format-var "%s"
+              docstr-show-type-name nil))
 
 (defun docstr-scala-config ()
   "Automatically configure style according to variable `docstr-scala-style'."
   (cl-case docstr-scala-style
+    (scaladoc (docstr-scala-config-scaladoc))
     (t (docstr-util-default-format))))
 
 ;;;###autoload
 (defun docstr-writers-scala (search-string)
   "Insert document string for Scala using SEARCH-STRING."
   (docstr-scala-config)
-  (docstr-writers-rust search-string))
+  (let* ((start (point)) (prefix docstr-rust-prefix)
+         (paren-param-list (docstr-writers--paren-param-list-behind search-string ":" t))
+         (param-types (nth 0 paren-param-list))
+         (param-vars (nth 1 paren-param-list))
+         (param-var-len (length param-vars))
+         (return-type-str (docstr-writers--return-type-behind search-string ":")))
+    (unless (= param-var-len 0)
+      (docstr-util-insert docstr-scala-prefix))
+    (docstr-writers--insert-param param-types param-vars prefix)
+    (docstr-writers--insert-return return-type-str '("void") prefix)
+    (docstr-writers-after start t t t)))
 
 (provide 'docstr-scala)
 ;;; docstr-scala.el ends here
