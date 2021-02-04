02d81db95caf5e99922bcb734f353fd5e4a05170
JenChieh <jcs090218@gmail.com>
2021-02-04 22:45:12 +0800

    Fix detect from go

--- langs/docstr-go.el
+++ langs/docstr-go.el
@@ -26,15 +26,13 @@
 
 (require 'docstr)
 
-(declare-function docstr-writers-c++ "ext:docstr-c++.el")
-
 (defcustom docstr-go-style 'godoc
   "Style specification for document string in Golang."
   :type '(choice (const :tag "No specify" nil)
                  (const :tag "Document String in Golang" godoc))
   :group 'docstr)
 
-(defcustom docstr-go-prefix "// "
+(defcustom docstr-go-prefix ""
   "Prefix you use on each newline."
   :type 'string
   :group 'docstr)
@@ -42,8 +40,7 @@
 (defun docstr-go-config-godoc ()
   "Configre for convention, GoDoc."
   (docstr-util-default-format)
-  (setq-local docstr-go-prefix "// "
-              docstr-format-type "{%s}"
+  (setq-local docstr-format-type "{%s}"
               docstr-format-var "%s -"
               docstr-show-type-name nil))
 
@@ -62,22 +59,13 @@
          (param-types (nth 0 paren-param-list))
          (param-vars (nth 1 paren-param-list))
          ;; Get the return data type.
-         (return-type-str (docstr-writers--return-type-behind search-string))
-         docstring-type)
-
+         (return-type-str (docstr-writers--return-type-behind search-string)))
     ;; Determine the docstring type.
-    (save-excursion
-      (backward-char 1)
-      (setq docstring-type (if (docstr-util-current-char-equal-p "/")
-                               'godoc 'javadoc)))
-
-    (cl-case docstring-type
-      (javadoc (docstr-writers-c++ search-string))
-      (godoc
-       (end-of-line)
-       (docstr-writers--insert-param param-types param-vars prefix)
-       (docstr-writers--insert-return return-type-str nil prefix)
-       (docstr-writers-after start t t t)))))
+    (if (docstr-util-multiline-comment-p) (setq prefix "") (setq prefix "// "))
+    (end-of-line)
+    (docstr-writers--insert-param param-types param-vars prefix)
+    (docstr-writers--insert-return return-type-str nil prefix)
+    (docstr-writers-after start t t t)))
 
 ;;;###autoload
 (defun docstr-trigger-golang (&rest _)
